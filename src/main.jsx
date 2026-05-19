4a947c565ed88ec0d59b4688413874380708a721
vip930sw <vip930sw@gmail.com>
2026-05-20 00:21:02 +0900

    Import auth form polish styles

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import './HomeResponsive.css'
 import './FooterDisclaimer.css'
 import './HeaderStartButtonPatch.css'
 import './MyPageBetaSimplify.css'
+import './AuthFormPolish.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
