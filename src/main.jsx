90d6ee222d0873eecf83fb5fda47d7c0bf08579d
vip930sw <vip930sw@gmail.com>
2026-05-20 01:21:30 +0900

    Step 134: load payment preparation patch

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './BetaNetworkFallback.js'
+import './PaymentPrepFlowPatch.js'
 import './index.css'
 import './HomeResponsive.css'
 import './FooterDisclaimer.css'
 import './HeaderStartButtonPatch.css'
 import './MyPageBetaSimplify.css'
 import './AuthFormPolish.css'
+import './PaymentPrepFlow.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
