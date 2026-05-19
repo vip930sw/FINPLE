42e955a00d4ca4455989c57b91d7981993cc4e3c
vip930sw <vip930sw@gmail.com>
2026-05-19 19:10:56 +0900

    Import lookup notice styles

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import './HomeResponsive.css'
 import './FooterDisclaimer.css'
+import './LookupNotice.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
