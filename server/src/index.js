1ab3537c24dc27f9a061a5965a1d7e4cd87818d9
vip930sw <vip930sw@gmail.com>
2026-05-20 20:10:11 +0900

    Step 143: mount payment routes

--- server/src/index.js
+++ server/src/index.js
@@ -4,6 +4,7 @@ import express from "express";
 
 import authRoutes from "./routes/authRoutes.js";
 import dbRoutes from "./routes/dbRoutes.js";
+import paymentRoutes from "./routes/paymentRoutes.js";
 import portfolioDbRoutes from "./routes/portfolioDbRoutes.js";
 import inquiryRoutes from "./routes/inquiryRoutes.js";
 
@@ -46,6 +47,7 @@ app.use(express.json({ limit: "1mb" }));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/db", dbRoutes);
+app.use("/api/payments", paymentRoutes);
 app.use("/api/account/portfolios", portfolioDbRoutes);
 app.use("/api/inquiries", inquiryRoutes);
 
